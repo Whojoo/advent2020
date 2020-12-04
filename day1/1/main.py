e0e03adf71d3a98820a90cdd4061f6387e8b87fb
Robin de Gier <rdgier@dearnova.nl>
2020-12-04 21:22:01 +0100

    completed day 1

--- day1/1/main.py
+++ day1/1/main.py
@@ -1,6 +1,6 @@
 def check(checker, num_list, start_idx, end_idx):
     if start_idx > end_idx:
-        return False
+        return -1
 
     idx_range = end_idx - start_idx
     target_idx = start_idx + int(idx_range / 2)
@@ -13,13 +13,11 @@ def check(checker, num_list, start_idx, end_idx):
 
     if total == 2020:
         print('YAY, answer is {} x {} = {}'.format(checker, target, checker * target))
-        return True
+        return target
     elif total > 2020:
         return check(checker, num_list, start_idx, target_idx - 1)
     else:
         return check(checker, num_list, target_idx + 1, end_idx)
-
-
 
 file_input = open("input")
 
@@ -29,8 +27,17 @@ num_list.sort()
 print(num_list)
 
 for num in num_list:
-    if check(num, num_list, 0, len(num_list) - 1):
+    if check(num, num_list, 0, len(num_list) - 1) > -1:
         print('Found it')
         break
 
-
+for idx1 in range(len(num_list)):
+    num1 = num_list[idx1]
+    for idx2 in range(idx1 + 1, len(num_list)):
+        num2 = num_list[idx2]
+        if num1 + num2 >= 2020 - num2:
+            break
+        sliced_list = num_list[idx2:]
+        num3 = check(num1 + num2, sliced_list, 0, len(sliced_list) - 1)
+        if num3 > -1:
+            print('Found it, {} x {} x {} = {}'.format(num1, num2, num3, num1 * num2 * num3))
