2cb54e6fe999bc7bf57333b1f2dc4e99794db392
Robin de Gier <rdgier@dearnova.nl>
2020-12-05 16:17:06 +0100

    day 3 challenge 2

--- day3/main.py
+++ day3/main.py
@@ -1,9 +1,9 @@
 
-def move(x, y, slope, lanes, lane_length):
+def move(x, y, slope, lanes, lane_length, move_x, move_y):
     if y >= lanes:
         return 0
 
-    trees = move(x + 3, y + 1, slope, lanes, lane_length)
+    trees = move(x + move_x, y + move_y, slope, lanes, lane_length, move_x, move_y)
 
     corrected_x = x % lane_length
     if slope[y][corrected_x] == '#':
@@ -26,4 +26,15 @@ for idx in range(lanes):
     for character in lane:
         slope[idx].append(character)
 
-print('trees {}'.format(move(0, 0, slope, lanes, lane_length)))
+move_1_1 = move(0, 0, slope, lanes, lane_length, 1, 1)
+move_3_1 = move(0, 0, slope, lanes, lane_length, 3, 1)
+move_5_1 = move(0, 0, slope, lanes, lane_length, 5, 1)
+move_7_1 = move(0, 0, slope, lanes, lane_length, 7, 1)
+move_1_2 = move(0, 0, slope, lanes, lane_length, 1, 2)
+
+print('move_1_1({})'.format(move_1_1))
+print('move_3_1({})'.format(move_3_1))
+print('move_5_1({})'.format(move_5_1))
+print('move_7_1({})'.format(move_7_1))
+print('move_1_2({})'.format(move_1_2))
+print('multiplied({})'.format(move_1_1 * move_3_1 * move_5_1 * move_7_1 * move_1_2))
