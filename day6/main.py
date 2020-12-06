4f2b87f4e4ccb9f01450c01f34f25afd8b8c8a59
Robin de Gier <rdgier@dearnova.nl>
2020-12-06 13:07:43 +0100

    day6 -2

--- day6/main.py
+++ day6/main.py
@@ -21,3 +21,31 @@ for count in counts:
     total_count += count
 
 print('total_count({})'.format(total_count))
+
+total_count_2 = 0
+
+temp_dict = {}
+temp_user_count = 0
+
+for line in input_list:
+    if line == '':
+        for key in temp_dict:
+            count = 1 if temp_dict[key] == temp_user_count else 0
+            total_count_2 += count
+
+        temp_dict = {}
+        temp_user_count = 0
+        continue
+
+    temp_user_count += 1
+    for char in line:
+        if char in temp_dict:
+            temp_dict[char] += 1
+        else:
+            temp_dict[char] = 1
+
+for key in temp_dict:
+    count = 1 if temp_dict[key] == temp_user_count else 0
+    total_count_2 += count
+
+print('total_count_2({})'.format(total_count_2))
