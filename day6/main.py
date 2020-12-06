e7889e0c1fd66f5a8e9119355edded713b7c7a60
Robin de Gier <rdgier@dearnova.nl>
2020-12-06 12:54:31 +0100

    day6 - 1

--- day6/main.py
+++ day6/main.py
@@ -1,3 +1,23 @@
 file_input = open("input")
 
 input_list = file_input.read().splitlines()
+
+temp_list = list()
+
+counts = []
+
+for line in input_list:
+    if line == '':
+        counts.append(len(set(temp_list)))
+        temp_list = list()
+        continue
+
+    temp_list = temp_list + list(line)
+
+counts.append(len(set(temp_list)))
+
+total_count = 0
+for count in counts:
+    total_count += count
+
+print('total_count({})'.format(total_count))
